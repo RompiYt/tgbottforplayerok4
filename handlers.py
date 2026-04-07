53b8fe4a9e77af6cdcf9e8c7b7f225b2d3133502
RompiYt <yarnikola2012@gmail.com>
2026-04-07 12:40:58 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -94,11 +94,132 @@ PRODUCTS = {
             "AgACAgIAAxkBAAONadTIN4v5CQqk82gSiKLXbCcR7NgAAtATaxu4LKBKWF9VFqOQk_cBAAMCAAN5AAM7BA",
             "AgACAgIAAxkBAAOGadTINzxdEGiW74MI7gNGmWLInacAAskTaxu4LKBKkXmGntN-XoABAAMCAAN5AAM7BA"
         ]
-    }
+    },
+    "Футболка:Aerounautica Мilitare":{
+        "name": "Футболка:Aerounautica Мilitare",
+        "category": "Футболка",
+        "material": "хлопок",
+        "colors": "белый🤍/черный🖤",
+        "price": "2690",
+        "sizes": "S,M,L,XL,2XL(46,48,50,52,54)",
+        "logo": "вышит",
+        "photos": [
+            "AgACAgIAAxkBAAMUadNvRVXH9NKocazjvEBGfcxN6k8AAkMVaxuHV5hK8UI5i71IETsBAAMCAAN5AAM7BA",
+        ]
+    },
+    "Футболка:Balenciaga":{
+        "name": "Футболка:Balenciaga",
+        "category": "Футболка",
+        "material": "хлопок",
+        "colors": "белый🤍/черный🖤",
+        "price": "2690",
+        "sizes": "S,M,L,XL,2XL(46,48,50,52,54)",
+        "logo": "вышит",
+        "photos": [
+            "AgACAgIAAxkBAAMUadNvRVXH9NKocazjvEBGfcxN6k8AAkMVaxuHV5hK8UI5i71IETsBAAMCAAN5AAM7BA",
+        ]
+    },
+    "Футболка:Tommy Hilfiger":{
+        "name": "Футболка:Tommy Hilfiger",
+        "category": "Футболка",
+        "material": "100% хлопок",
+        "colors": "белый🤍",
+        "price": "2190",
+        "sizes": "S,M,L,XL,2XL(46,48,50,52,54)",
+        "logo": "вышит",
+        "photos": [
+            "AgACAgIAAxkBAAMUadNvRVXH9NKocazjvEBGfcxN6k8AAkMVaxuHV5hK8UI5i71IETsBAAMCAAN5AAM7BA",
+        ]
+    },
+    "Футболка:Emporio Armani":{
+        "name": "Футболка:Emporio Armani",
+        "category": "Футболка",
+        "material": "хлопок",
+        "colors": "черный🖤",
+        "price": "2190",
+        "sizes": "S,M,L,XL,2XL(46,48,50,52,54)",
+        "logo": "термонаклейка",
+        "photos": [
+            "AgACAgIAAxkBAAMUadNvRVXH9NKocazjvEBGfcxN6k8AAkMVaxuHV5hK8UI5i71IETsBAAMCAAN5AAM7BA",
+        ]
+    },
+    "Футболка:Hugo Dobermann":{
+        "name": "Футболка:Hugo Dobermann",
+        "category": "Футболка",
+        "material": "100% хлопок",
+        "colors": "белый🤍/черный🖤",
+        "price": "2190",
+        "sizes": "S,M,L,XL,2XL(46,48,50,52,54)",
+        "logo": "графический принт",
+        "photos": [
+            "AgACAgIAAxkBAAMUadNvRVXH9NKocazjvEBGfcxN6k8AAkMVaxuHV5hK8UI5i71IETsBAAMCAAN5AAM7BA",
+        ]
+    },
+    "Футболка:Hugo Full House":{
+        "name": "Футболка:Hugo Full House",
+        "category": "Футболка",
+        "material": "100% хлопок",
+        "colors": "черный🖤",
+        "price": "2550",
+        "sizes": "S,M,L,XL,2XL(46,48,50,52,54)",
+        "logo": "вышит",
+        "photos": [
+            "AgACAgIAAxkBAAMUadNvRVXH9NKocazjvEBGfcxN6k8AAkMVaxuHV5hK8UI5i71IETsBAAMCAAN5AAM7BA",
+        ]
+    },
+    "Футболка:Polo Ralph lauren chief keef":{
+        "name": "Футболка:Polo Ralph lauren chief keef",
+        "category": "Футболка",
+        "material": "хлопок",
+        "colors": "черный🖤",
+        "price": "1990",
+        "sizes": "S,M,L,XL(46,48,50,52)",
+        "logo": "вышит",
+        "photos": [
+            "AgACAgIAAxkBAAMUadNvRVXH9NKocazjvEBGfcxN6k8AAkMVaxuHV5hK8UI5i71IETsBAAMCAAN5AAM7BA",
+        ]
+    },
+    "Футболка:Tommy Hilfiger":{
+        "name": "Футболка:Tommy Hilfiger",
+        "category": "Футболка",
+        "material": "100% хлопок",
+        "colors": "белый🤍",
+        "price": "1990",
+        "sizes": "S,M,L,XL,2XL(46,48,50,52,54)",
+        "logo": "вышит",
+        "photos": [
+            "AgACAgIAAxkBAAMUadNvRVXH9NKocazjvEBGfcxN6k8AAkMVaxuHV5hK8UI5i71IETsBAAMCAAN5AAM7BA",
+        ]
+    },
+    "Футболка:Hugo":{
+        "name": "Футболка:Hugo ",
+        "category": "Футболка",
+        "material": "100% хлопок",
+        "colors": "черный🖤",
+        "price": "1990",
+        "sizes": "S,M,L,XL,2XL(46,48,50,52,54)",
+        "logo": "графический принт",
+        "photos": [
+            "AgACAgIAAxkBAAMUadNvRVXH9NKocazjvEBGfcxN6k8AAkMVaxuHV5hK8UI5i71IETsBAAMCAAN5AAM7BA",
+        ]
+    },
+    "Футболка:Guess":{
+        "name": "Футболка:Guess",
+        "category": "Футболка",
+        "material": "100% хлопок",
+        "colors": "черный🖤",
+        "price": "1990",
+        "sizes": "S,M,L,XL,2XL(46,48,50,52,54)",
+        "logo": "вышит",
+        "photos": [
+            "AgACAgIAAxkBAAMUadNvRVXH9NKocazjvEBGfcxN6k8AAkMVaxuHV5hK8UI5i71IETsBAAMCAAN5AAM7BA",
+        ]
+    },
 }
 
 CATEGORIES = {
     "Зип-худи": ["Зип-худи:Balenciaga", "Зип-худи:Polo ralph lauren", "Зип-худи:Lacoste", "Зип-худи:Karl Lagerfeld", "Зип-худи:Burberry"]
+    "Футболка": ["Футболка:Aerounautica Мilitare", "Футболка:Balenciaga", "Футболка:Tommy Hilfiger", "Футболка:Emporio Armani", "Футболка:Hugo Dobermann", "Футболка:Hugo Full House", "Футболка:Polo Ralph lauren chief keef", "Футболка:Tommy Hilfiger", "Футболка:Hugo", "Футболка:Guess"]
 }
 
 user_photo_index = {}
@@ -123,6 +244,7 @@ async def show_catalog(callback: CallbackQuery):
         "📦 Наш каталог товаров:\nВыберите категорию:",
         reply_markup=InlineKeyboardMarkup(inline_keyboard=[
             [InlineKeyboardButton(text="🧥 Зип худи", callback_data="Зип-худи")]
+            [InlineKeyboardButton(text="👕 Футболка", callback_data="Футболка")]
         ])
     )
     await callback.answer()
@@ -161,7 +283,7 @@ async def open_product(callback: CallbackQuery):
 @router.callback_query(F.data.startswith("next_photo:"))
 async def next_photo(callback: CallbackQuery):
     user_id = callback.from_user.id
-    product_id = ":".join(callback.data.split(":")[1:])  # 💥 фикс
+    product_id = ":".join(callback.data.split(":")[1:])  
     index = user_photo_index.get(user_id, {}).get(product_id, 0)
     if index < len(PRODUCTS[product_id]["photos"]) - 1:
         index += 1
@@ -172,7 +294,7 @@ async def next_photo(callback: CallbackQuery):
 @router.callback_query(F.data.startswith("prev_photo:"))
 async def prev_photo(callback: CallbackQuery):
     user_id = callback.from_user.id
-    product_id = ":".join(callback.data.split(":")[1:])  # 💥 фикс
+    product_id = ":".join(callback.data.split(":")[1:])  
     index = user_photo_index.get(user_id, {}).get(product_id, 0)
     if index > 0:
         index -= 1
@@ -195,7 +317,7 @@ async def back_to_category(callback: CallbackQuery):
     ]
     buttons.append([InlineKeyboardButton(text="⬅️ Назад", callback_data="back_to_catalog")])
 
-    await callback.message.delete()  # 💥 ВАЖНО
+    await callback.message.delete()  
 
     await callback.message.answer(
         f"Категория: {category_id}\nВыберите товар:",
